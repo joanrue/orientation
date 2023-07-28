1d5400939a0a506f093bb98405d25272b77542b3
Joan Rue Queralt <joan.rue.q@gmail.com>
2023-07-28 14:42:53 +0200

    clean & doc & remove eigendecomposition & use pasta CT-reconstruction

--- __init__.py
+++ __init__.py
@@ -4,30 +4,35 @@ from eigh import eigh
 from lode import lode_coords
 import tifffile
 import matplotlib.pyplot as plt
-y = tifffile.imread("data/membrane.tif").astype("float32") / 255
-#y = skimage.data.cells3d()[:,0]
-# z_slice = 30
-# plt.imshow(y[z_slice]), plt.show()
 
-st = StructureTensor(arg_shape=y.shape)
-x = st(y.ravel())
-x = st.unravel(x)
-# for i in range(6):
-#      tifffile.imwrite(f"data/membrane_st{i}.tif", (x[i] * 255).astype("<u1"))
+# Define input path and parameters
+input_path = "data/SlicesY_bin4.tif"
+sampling = (1., 1., 1.) # Change to correct values, are they isotropic voxels?
+sigma = 2.5 # for structure tensor
 
-x = x.transpose(1, 2, 3, 0)
-x2 = np.zeros((*x.shape[:-1], 9))
-x2[..., [0, 1, 2, 4, 5, 8]] = x
-w, v = eigh(x2.reshape(-1, 9), arg_shape=(3, 3))
-w = w.reshape(*y.shape, 3)
-# for i in range(3):
-#      tifffile.imwrite(f"data/membrane_w{i}.tif", (((w[..., i] - w[..., i].min()) / (w[..., i] - w[..., i].min()).max()) * 255).astype("<u1"))
+# Load image
+image = tifffile.imread(input_path).astype("float32")
+image /= image.max()
 
-lode = lode_coords(v.reshape(-1, 3, 3))
-lode = lode.reshape(*y.shape, 3)
+# Define JIT-compiled structure tensor operator
+structens = StructureTensor(arg_shape=image.shape,
+                            smooth_sigma=sigma,
+                            sampling=sampling)
 
+# Apply structure tensor operator
+st = structens.unravel(structens(image.ravel()))
+st = st.transpose(1, 2, 3, 0)
+# Reshape for Lode coordinate computations
+st2 = np.zeros((*st.shape[:-1], 9))
+st2[..., [0, 1, 2, 4, 5, 8]] = st
+
+# Compute Lode coordinates
+lode = lode_coords(st2.reshape(-1, 3, 3))
+lode = lode.reshape(*image.shape, 3)
+
+# Save result
 for i in range(3):
-     # tifffile.imwrite(f"data/membrane_lode{i}.tif", (lode[..., i] * 255).astype("<u1"))
-     tifffile.imwrite(f"data/membrane_lode{i}.tif", lode[..., i])
-     tifffile.imwrite(f"data/membrane_lode{i}_norm.tif", (((lode[..., i] - np.nanmin(lode[..., i])) / np.nanmax((lode[..., i] - np.nanmin(lode[..., i])))) * 255).astype("<u1"))
+     tifffile.imwrite(f"data/SliceY_lode{i}.tif", lode[..., i])
+     tifffile.imwrite(f"data/SliceY_lode{i}_norm.tif", (((lode[..., i] - np.nanmin(lode[..., i])) / np.nanmax(
+          (lode[..., i] - np.nanmin(lode[..., i])))) * 255).astype("<u1"))
 print("Done!")
